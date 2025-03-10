c4e7a9f30f45648e710a7363656f4d4b421a527c
Travis Westura <twestura@gmail.com>
2025-03-10 09:29:50 -0400

    Lots of updates

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@
 // TODO fix project settings so don't need the `.js` and unresolved error
 // ignores.
 
-import { exists, fstat } from "fs";
+//import { exists, fstat } from "fs";
 import fetch from "node-fetch";
 import { env } from "process";
 import { Client } from "tmi.js";
@@ -15,11 +15,20 @@ import { EnvValues, load_env } from "./load_env.js";
 /** Link to join my Discord server. */
 const LINK_DISCORD = "discord.gg/N2qSkbk";
 
+/** Link to my Instagram page. */
+const LINK_INSTAGRAM = "https://www.instagram.com/twestaoe/";
+
 /** Link to my Patreon page. */
 const LINK_PATREON = "patreon.com/twest";
 
+/** Link to my TikTok account. */
+const LINK_TIKTOK = "tiktok.com/@twestaoe";
+
 /** Link to my Twitter account. */
 const LINK_TWITTER = "twitter.com/TravisWestura";
+
+/** Link to my website. */
+const LINK_WEBSITE = "twestaoe.net";
 
 /** Link to my YouTube channel. */
 const LINK_YOUTUBE = "youtube.com/TWestYT";
@@ -37,6 +46,42 @@ const GIVEAWAY_FILE = "resources/giveaway.csv";
 const GIVEAWAY_MSG =
   "Vitalis is giving away a copy of Aoe 1, 2, or 3 DE or any DLC (your choice). Type !enter to enter.";
 
+/** Message about Kamigawa's pizzeria. */
+const PIZZA_MESSAGE =
+  "Check out Kamigawa's Pizzeria in Berlin: https://www.byebyecavaliere.de";
+
+/** Message with a Discount code for the NAC watch party. */
+const NAC_DISCOUNT =
+  "Want to come to the NAC Watch Party? Use my discount code for 20% off: NOBLETWEST20";
+
+/** Message linking to the Instagram post of my Male Villager cosplay. */
+const COSPLAY =
+  "I did a Male Villager cosplay: https://www.instagram.com/p/DCW-74uxFHh";
+
+/** Message about garrison tickets. */
+const GARRISON_TICKETS =
+  "Join The Garrison in Hamburg, Germany! Whether as a player or a spectator, be part of the action. Get your tickets here: https://www.eventbrite.com/e/the-garrison-tickets-1043494268447";
+
+/** Message about the Garrison. */
+const GARRISON_MESSAGE =
+  "Get more information about The Garrison Offline Event in Hamburg, Germany on liquipedia or in our handbook: https://data.being-cloud.com/s/sRQBaW9q8nGfc68  https://liquipedia.net/ageofempires/The_Garrison";
+
+/** Link to the Garrison Discord. */
+const GARRISON_DISCORD =
+  "Join the official The Garrison Discord to stay informed about the event: https://discord.gg/rSdW4DCPMw";
+
+/** Message about TTL4. */
+const TTL4_MESSAGE =
+  "T90 Titans League is hosted by T90Official (https://twitch.tv/T90Official) & sponsored by World’s Edge. You can read more about it here - https://liquipedia.net/ageofempires/T90_Titans_League/4/Platinum_League";
+
+/** Message about the France-Germany charity. */
+const CHARITY =
+  "All donations go to DKMS (German Bone Marrow Donor Center) to support the fight against blood cancer. https://tinyurl.com/gerfradkms";
+
+/** WH2 message. */
+const WALLHALLA2 =
+  "Wallhalla 2 hosted by JonSlow (https://www.twitch.tv/jonslow_) and Ornlu (https://www.twitch.tv/ornlu_aoe).";
+
 /**
  * Utility function to ensure exhaustiveness of switch statements on literal
  * union types.
@@ -48,23 +93,27 @@ function assert_unreachable(_: never): never {
 
 /** The names of bot commands. Sorted lexicographically. */
 const command_names = [
-  "civs",
+  "charity",
   "commands",
-  "double",
+  "cosplay",
   "discord",
-  "draw",
-  "donate",
-  "elo",
-  "enter",
-  "giveaway",
-  "map",
-  "match",
+  "garrisondiscord",
+  "instagram",
+  // "draw",
+  // "enter",
+  // "giveaway",
+  "nacdiscount",
   "patreon",
-  "rank",
-  "rating",
-  "start",
-  "stop",
+  "pizza",
+  // "start",
+  // "stop",
+  "thegarrison",
+  "tickets",
+  "tiktok",
+  "ttl",
   "twitter",
+  "website",
+  "wh2",
   "youtube",
 ] as const;
 
@@ -228,10 +277,12 @@ const create_bot: (env: EnvValues) => void = () => {
     component,
     arg
   ) => {
+    const headers = new Headers({ "User-Agent": "TWestBot/5.5" });
     const query = arg === "" ? `steam_id=${id_steam}` : `search=${arg}`;
-    const url = `https://aoe2.net/api/nightbot/${component}${query}`;
+    const url = `https://aoe2.net/api/nightbot/${component}game=aoe2de&${query}`;
     try {
       const response = await fetch(url);
+      if (response.status !== 200) console.log(response); // TODO remove after debugging
       return response.status === 200 ? response.text() : FETCH_ERR_MSG;
     } catch (_) {
       return FETCH_ERR_MSG;
@@ -247,7 +298,7 @@ const create_bot: (env: EnvValues) => void = () => {
     // Skips the command if called multiple times within a short period of time.
     if (
       last_execution.has(cmd.prefix) &&
-      cmd.prefix !== "enter" &&
+      // cmd.prefix !== "enter" &&
       Date.now() - last_execution.get(cmd.prefix) <= COOLDOWN_DELAY
     ) {
       console.log(`Command ${cmd.prefix} on cooldown.`);
@@ -256,79 +307,83 @@ const create_bot: (env: EnvValues) => void = () => {
     last_execution.set(cmd.prefix, Date.now());
 
     switch (cmd.prefix) {
-      case "double":
-        client.say(
-          channel,
-          "Double Cup 3 is a community-funded multiple-tc start tournament hosted by Huehuecoyotl22, Elhyar & DaisyChain. If you want to contribute to reaching the prize pool goal of $700, you can donate here: streamlabs.com/huehuecoyotl22/tip More information: liquipedia.net/ageofempires/Double_Cup/3"
-        );
-        break;
-      case "donate":
-        client.say(
-          channel,
-          "Donate to LT's Starlight Fundraiser here: https://donate.tiltify.com/@liltrouble__/wololo-starlight"
-        );
+      case "charity":
+        client.say(channel, CHARITY);
         break;
       case "commands":
         client.say(channel, list_commands());
         break;
-      case "civs":
-        client.say(channel, await fetch_url("civs?", cmd.arg));
+      case "cosplay":
+        client.say(channel, COSPLAY);
         break;
       case "discord":
         client.say(channel, LINK_DISCORD);
         break;
-      case "draw":
-        if (tags["user-id"] != env.id_twitch) break; // Only I may draw.
-        const n = parseInt(cmd.arg);
-        if (isNaN(n) || n <= 0) {
-          console.log(`${n} is not a valid number of winners.`);
-          break;
-        }
-        const winners = draw_winners(n);
-        console.log(`winners: ${winners}`); // Log twitch IDs to console.
-        client.say(channel, winner_msg(winners));
+      case "garrisondiscord":
+        client.say(channel, GARRISON_DISCORD);
         break;
-      case "enter":
-        if (!is_giveaway_live) break;
-        if (tags["user-id"] !== undefined && tags["display-name"] !== undefined)
-          add_entrant(tags["user-id"], tags["display-name"]);
+      // case "draw":
+      //   if (tags["user-id"] != env.id_twitch) break; // Only I may draw.
+      //   const n = parseInt(cmd.arg);
+      //   if (isNaN(n) || n <= 0) {
+      //     console.log(`${n} is not a valid number of winners.`);
+      //     break;
+      //   }
+      //   const winners = draw_winners(n);
+      //   console.log(`winners: ${winners}`); // Log twitch IDs to console.
+      //   client.say(channel, winner_msg(winners));
+      //   break;
+      // case "enter":
+      //   if (!is_giveaway_live) break;
+      //   if (tags["user-id"] !== undefined && tags["display-name"] !== undefined)
+      //     add_entrant(tags["user-id"], tags["display-name"]);
+      //   break;
+      // case "giveaway":
+      //   client.say(channel, GIVEAWAY_MSG);
+      //   break;
+      // case "start":
+      //   if (tags["user-id"] != env.id_twitch) break; // Only I may start.
+      //   is_giveaway_live = true;
+      //   break;
+      // case "stop":
+      //   if (tags["user-id"] != env.id_twitch) break; // Only I may stop.
+      //   is_giveaway_live = false;
+      //   break;
+      case "instagram":
+        client.say(channel, LINK_INSTAGRAM);
         break;
-      case "giveaway":
-        client.say(channel, GIVEAWAY_MSG);
-        break;
-      case "start":
-        if (tags["user-id"] != env.id_twitch) break; // Only I may start.
-        is_giveaway_live = true;
-        break;
-      case "stop":
-        if (tags["user-id"] != env.id_twitch) break; // Only I may stop.
-        is_giveaway_live = false;
-        break;
-      case "map":
-        client.say(channel, await fetch_url("map?", cmd.arg));
-        break;
-      case "match":
-        client.say(
-          channel,
-          await fetch_url("match?color=true&flag=false&", cmd.arg)
-        );
+      case "nacdiscount":
+        client.say(channel, NAC_DISCOUNT);
         break;
       case "patreon":
         client.say(channel, LINK_PATREON);
         break;
+      case "pizza":
+        client.say(channel, PIZZA_MESSAGE);
+        break;
+      case "thegarrison":
+        client.say(channel, GARRISON_MESSAGE);
+        break;
+      case "tickets":
+        client.say(channel, GARRISON_TICKETS);
+        break;
+      case "tiktok":
+        client.say(channel, LINK_TIKTOK);
+        break;
+      case "ttl":
+        client.say(channel, TTL4_MESSAGE);
+        break;
       case "twitter":
         client.say(channel, LINK_TWITTER);
         break;
+      case "website":
+        client.say(channel, LINK_WEBSITE);
+        break;
+      case "wh2":
+        client.say(channel, WALLHALLA2);
+        break;
       case "youtube":
         client.say(channel, LINK_YOUTUBE);
-        break;
-      case "elo":
-      case "rank":
-      case "rating":
-        client.say(
-          channel,
-          await fetch_url("rank?leaderboard_id=3&flag=false&", cmd.arg)
-        );
         break;
       default:
         assert_unreachable(cmd.prefix);
@@ -350,5 +405,4 @@ const main = () => {
   create_bot(env.val);
 };
 
-// Runs the bot by calling `main`.
-main();
+main(); // Runs the bot by calling `main`.
